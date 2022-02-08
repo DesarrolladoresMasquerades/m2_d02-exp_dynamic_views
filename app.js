3eb5c96ab09f32e53c6115dc31414577122c90c3
marcosantonastasi <marco.santonastasi@gmail.com>
2022-02-08 13:02:25 +0100

    starter code

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res, next) => res.send("Hello there!"));
 
-app.get("/homepage", (req, res, next) =>
-  res.sendFile(__dirname + "/views/home.html")
+app.get("/home", (req, res, next) =>
+res.sendFile(__dirname + "/views/home.html")
 );
 
 app.get("/about", (req, res, next) =>
-  res.sendFile(__dirname + "/views/about.html")
+res.sendFile(__dirname + "/views/about.html")
 );
+
+app.get("/", (req, res, next) => res.send("Hello there!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🏃‍ on port 3000"));
